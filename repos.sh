7a3901fa57b9934b08067ba9492e8a1d4ccca07b
Joseph <calledtoconstruct@gmail.com>
2019-05-03 23:17:38 +0000

    Added repos.sh

---
+++ repos.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/calledtoconstruct/lambdabot
+git clone https://github.com/calledtoconstruct/maze
+git clone https://github.com/calledtoconstruct/sudoku
+git clone https://github.com/calledtoconstruct/kata-checkout-order-total
+git clone https://github.com/calledtoconstruct/tower_of_hanoi
+git clone https://github.com/calledtoconstruct/game-of-life
--- setup.sh
+++ setup.sh
@@ -1,9 +1,8 @@
 sudo apt update
 sudo apt upgrade
-# sudo add-apt-repository ppa:hvr/ghc
-# curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) universe"
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 sudo apt update
 sudo apt install code nodejs npm cabal-install
+chmod +x repos.sh
