7972650acef7a4eee9af1229e75c69498e0ce343
Joseph <calledtoconstruct@gmail.com>
2019-05-03 23:19:02 +0000

    Fix path

--- setup.sh
+++ setup.sh
@@ -5,4 +5,4 @@ wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 sudo apt update
 sudo apt install code nodejs npm cabal-install
-chmod +x repos.sh
+chmod +x ./setup/repos.sh
