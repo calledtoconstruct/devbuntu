e1a5edc2c815a92a38cf0c4ba2b95a35afd21dbf
Joseph Woolley <joe@calledtoconstruct.net>
2019-05-08 16:28:04 +0000

    Create vbox.sh

---
+++ vbox.sh
@@ -0,0 +1,4 @@
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install virtualbox-6.0
