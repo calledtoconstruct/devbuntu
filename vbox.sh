0225939227e79517ebe7ad801f233164011f5c05
Joseph Woolley <joe@calledtoconstruct.net>
2019-05-08 19:56:50 +0000

    Update vbox.sh

--- vbox.sh
+++ vbox.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
 wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
 sudo apt-get update
