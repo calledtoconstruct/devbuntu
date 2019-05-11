f31ce99a895896bc246950f5529265f1b8dbe8c6
Joseph Woolley <calledtoconstruct>
2019-05-11 13:54:12 +0000

    Tweak setup.

--- setup.sh
+++ setup.sh
@@ -5,4 +5,5 @@ wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 sudo apt update
 sudo apt install code nodejs npm cabal-install
-chmod +x ./setup/repos.sh
+sudo npm i -g npm
+chmod +x ./setup/*.sh
