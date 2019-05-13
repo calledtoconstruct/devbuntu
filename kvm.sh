589abb9ace162465bdbb16908aae63445e0335c5
Joseph Woolley <calledtoconstruct@gmail.com>
2019-05-13 13:41:33 +0000

    Update kvm.sh

--- kvm.sh
+++ kvm.sh
@@ -4,5 +4,6 @@ sudo systemctl start libvirtd.service
 sudo systemctl status libvirtd.service
 sudo curl -LO https://storage.googleapis.com/minikube/releases/latest/docker-machine-driver-kvm2 && sudo install docker-machine-driver-kvm2 /usr/local/bin/
 minikube config set vm-driver kvm2
+minikube config set disk-size "8g"
 sudo usermod -a -G libvirt $(whoami)
 newgrp libvirt
