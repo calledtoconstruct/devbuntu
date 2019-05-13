dcde89c864d8f3fdb4f297519e3eb261a6037402
Joseph Woolley <calledtoconstruct@gmail.com>
2019-05-13 12:35:41 +0000

    Update kvm.sh

--- kvm.sh
+++ kvm.sh
@@ -4,3 +4,5 @@ sudo systemctl start libvirtd.service
 sudo systemctl status libvirtd.service
 sudo curl -LO https://storage.googleapis.com/minikube/releases/latest/docker-machine-driver-kvm2 && sudo install docker-machine-driver-kvm2 /usr/local/bin/
 minikube config set vm-driver kvm2
+sudo usermod -a -G libvirt $(whoami)
+newgrp libvirt
