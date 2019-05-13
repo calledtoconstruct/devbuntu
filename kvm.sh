50513180916b84e9d7cc8bd8107ae9cf8d71c0bc
Joseph Woolley <calledtoconstruct@gmail.com>
2019-05-13 12:31:03 +0000

    Create kvm.sh

---
+++ kvm.sh
@@ -0,0 +1,6 @@
+sudo apt install libvirt-clients libvirt-daemon-system qemu-kvm
+sudo systemctl enable libvirtd.service
+sudo systemctl start libvirtd.service
+sudo systemctl status libvirtd.service
+sudo curl -LO https://storage.googleapis.com/minikube/releases/latest/docker-machine-driver-kvm2 && sudo install docker-machine-driver-kvm2 /usr/local/bin/
+minikube config set vm-driver kvm2
