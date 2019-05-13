bffa91a4302b0292f30d0e247d2f3c56ad298a27
Joseph Woolley <calledtoconstruct@gmail.com>
2019-05-13 13:14:15 +0000

    Update clean-up.sh

--- clean-up.sh
+++ clean-up.sh
@@ -1,6 +1,6 @@
 sudo rm /usr/share/applications/ubuntu-amazon-default.desktop
 sudo rm /usr/share/unity-webapps/userscripts/unity-webapps-amazon/Amazon.user.js
 sudo rm /usr/share/unity-webapps/userscripts/unity-webapps-amazon/manifest.json
-sudo apt remove --purge gnome-todo thunderbird* simple-scan libreoffice*
-sudo apt autoremove
+sudo apt remove --purge -y gnome-todo thunderbird* simple-scan libreoffice*
+sudo apt autoremove -y
 sudo apt autoclean
