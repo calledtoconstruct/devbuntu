4192557b9d348e2f133bed848f00a428e1aa89e1
Joseph <joe@calledtoconstruct.net>
2019-06-01 11:43:45 +0000

    Clean-up and optimization.

--- setup.sh
+++ setup.sh
@@ -1,9 +1,7 @@
-sudo apt update
-sudo apt upgrade
 sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) universe"
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 sudo apt update
-sudo apt install -y code nodejs npm cabal-install
+sudo apt -y upgrade
+sudo apt -y install code nodejs npm cabal-install
 sudo npm i -g npm
-chmod +x ./setup/*.sh
--- sign-kernel.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd ../$kernelver/$arch/module/
-
-for kernel_object in *ko; do
-     echo "Signing kernel_object: $kernel_object"
-    /usr/src/linux-headers-$kernelver/scripts/sign-file sha256 /root/MOK.priv /root/MOK.der "$kernel_object";
-done
