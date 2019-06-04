c5da926fa0c6e70b4ef4ecb673e3e2d35f7dcb1c
Joseph <joe@calledtoconstruct.net>
2019-06-04 10:07:41 +0000

    Adding mongo-db install script.

---
+++ mongodb.sh
@@ -0,0 +1,5 @@
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
+echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
+sudo apt update
+sudo apt -y install mongodb-org
+sudo service mongod start
