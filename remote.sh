c4d5ee675340677db89492db9f6754453debbba8
Joseph <joe@calledtoconstruct.net>
2019-07-25 05:14:01 +0000

    Adding password change and logout to remote script.

--- remote.sh
+++ remote.sh
@@ -6,3 +6,6 @@ wget https://github.com/Palakis/obs-ndi/releases/download/4.6.0/libndi3_3.8.0-1_
 sudo dpkg -i obs-ndi_4.5.3-1_amd64.deb
 sudo dpkg -i libndi3_3.8.0-1_amd64.deb
 sudo cp /usr/lib/obs-plugins/* /usr/lib/x86_64-linux-gnu/obs-plugins/
+sudo cp ./setup/Xwrapper.config /etc/X11/
+passwd
+gnome-session-quit
