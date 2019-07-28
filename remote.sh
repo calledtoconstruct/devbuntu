16cb1cbfa94ff6047d0bc763ee360bebe993262b
Joseph <joe@calledtoconstruct.net>
2019-07-28 17:03:00 +0000

    Updating remote script to correctly call passwd with elevated privileges.

--- remote.sh
+++ remote.sh
@@ -7,5 +7,5 @@ sudo dpkg -i obs-ndi_4.5.3-1_amd64.deb
 sudo dpkg -i libndi3_3.8.0-1_amd64.deb
 sudo cp /usr/lib/obs-plugins/* /usr/lib/x86_64-linux-gnu/obs-plugins/
 sudo cp ./setup/Xwrapper.config /etc/X11/
-passwd
+sudo passwd
 gnome-session-quit
