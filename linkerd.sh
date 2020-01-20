88641f17bed5f4001670132885370e4230479541
Joseph <joe@calledtoconstruct.net>
2020-01-20 05:48:47 +0000

    Remove super user prefix as it is no longer required.

--- linkerd.sh
+++ linkerd.sh
@@ -1,4 +1,4 @@
 curl -sL https://run.linkerd.io/install | sh
 sudo cp $HOME/.linkerd2/bin/linkerd /usr/local/bin/linkerd
-sudo linkerd install | sudo minikube kubectl -- apply -f -
-sudo linkerd check
+linkerd install | minikube kubectl -- apply -f -
+linkerd check
